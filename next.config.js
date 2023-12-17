dffe49d864c888f25143a947acdc4ad172eee20c
Darlin Ruben Nina C. <chinausto@gmail.com>
2023-12-16 22:26:44 -0400

    template_app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-// next.config.js
-module.exports = {
-  // Configuración adicional aquí
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <header></header>
+      <main className="container mx-auto p-5">
+        <h1>hola</h1>
+      </main>
+      <footer className="absolute bottom-0"></footer>
+    </>
+  );
+}
